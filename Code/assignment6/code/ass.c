7fe75f010d23f27873f65c578d3383a296486eea
Yuto Uematsu <utomatsu@gmail.com>
2020-09-11 21:33:17 +0900

    master

--- Code/assignment6/code/ass.c
+++ Code/assignment6/code/ass.c
@@ -39,7 +39,7 @@ void Simal_Read(double a[SIZE][SIZE], double b[SIZE]) { /* 連立方程式の読
         scanf("%lf", &(b[i]));
         printf("i:%.2d min:%.3d  max:%.3d\n", i, j_min[i], j_max[i]);
     }
-        printf("\n");
+    printf("\n");
 }
 
 void Simal_Write(double a[SIZE][SIZE], double b[SIZE]) { /* 連立方程式の書き出し */
@@ -91,7 +91,7 @@ int main(int argc, char **argv) {
     if (n == 0) {  // nが0の時の処理
         printf("Size: ");
         scanf("%d", &n);
-        printf("%d\n",n);
+        printf("%d\n", n);
         printf("\n");
     }
     if (n > SIZE) { /* エラー処理 (n > SIZE) */
@@ -101,66 +101,63 @@ int main(int argc, char **argv) {
 
     for (i = 0; i < n; i++) vecX[i] = C; /* vecX準備；初期値代入 */
 
-    Simal_Read(matA, vecB); /* 問題の読み込み */
-    if (verbose) { // -vならば
+    Simal_Read(matA, vecB);      /* 問題の読み込み */
+    if (verbose) {               // -vならば
         Simal_Write(matA, vecB); /* 清書 */
     }
 
     // 係数マトリクスの下準備：各行を対角成分で割る
     for (j = 0; j < n; j++) {
-        C = matA[j][j]; // 対角成分事態に操作は不要
-        for (k = j_min[i]; k <= j_max[i]; k++) { // 範囲から0成分を除外
-            if (j != k) { //対角でない場合
-                matA[j][k] = -matA[j][k] / C; //後でmatA[j][k]を足すだけで良いようにしている
+        int c = 0;
+        C = matA[j][j];                              // 対角成分事態に操作は不要
+        for (k = j_min[j]; k < n - j_max[j]; k++) {  // 範囲から0成分を除外
+            c++;
+            if (j != k) {                      //対角でない場合
+                matA[j][k] = -matA[j][k] / C;  //後でmatA[j][k]を足すだけで良いようにしている
             }
         }
         vecB[j] = vecB[j] / C;
+        // printf("j:%d c:%d\n",j,c);
     }
 
     times(&tfrom); /* 計算時間計測開始 */
 
-    cc = 0; // ただの回数カウンタ
-    cont = 1; // 収束判定用
-    while (cont && (cc < LIMIT)) { // 収束してない && カウンタが上限回数に達していない(200未満)
-        if (!map) { // -m で無い場合の処理
+    cc = 0;                         // ただの回数カウンタ
+    cont = 1;                       // 収束判定用
+    while (cont && (cc < LIMIT)) {  // 収束してない && カウンタが上限回数に達していない(200未満)
+        if (!map) {                 // -m で無い場合の処理
             printf("%03d: ", cc);
-            V_Write(vecX); /* 暫定解の書き出し */
+            V_Write(vecX);  // 暫定解の書き出し
         }
 
-        for (j = 0; j < n; j++) { // Jacobi 法による計算
-            Sum = vecB[j]; // 初期解(対角成分で除済み)
-            for (k = 0; k < n; k++) {
+        for (j = 0; j < n; j++) {                        // Jacobi 法による計算
+            Sum = vecB[j];                               // 初期解(対角成分で除済み)
+            for (k = j_min[j]; k < n - j_max[j]; k++) {  // 範囲から0成分を除外
                 if (j != k) {
-                    Sum += matA[j][k] * vecX[k]; /* 積和計算 */ // matA の符号は反転済み
+                    Sum += matA[j][k] * vecX[k];  // 積和計算, matA の符号は反転済み
                 }
             }
-            vecT[j] = Sum; // 次の解として保存
+            vecT[j] = Sum;  // 次の解として保存
         }
         cc++;
 
-        if (map) { 
+        if (map) {
             printf("%03d: ", cc);
         }
         cont = 0;
-        for (i = 0; i < n; i++) { /* 収束状況の確認 */
-            p = 999; // p は精度，初期値999(めっちゃ精度悪い)
-            if (vecT[i] != 0 && (p = fabs((vecT[i] - vecX[i]) / vecT[i])) > prec) { // 次の解との相対誤差を計算
+        for (i = 0; i < n; i++) {                                                    /* 収束状況の確認 */
+            p = 999;                                                                 // p は精度，初期値999(めっちゃ精度悪い)
+            if (vecT[i] != 0 && (p = fabs((vecT[i] - vecX[i]) / vecT[i])) > prec) {  // 次の解との相対誤差を計算
                 cont = 1;
             }
-            if (map) { /* 有効桁数マップの作成 */
-                dig = (long)(-log(p) / log(10.0) + 0.5); // 少数以下の桁数を計算
-                if (dig > 35) { // 35よりも大きければ35に縮める
-                    dig = 35;
-                }
-                if (dig < 0) { // 0未満なら0に収める
-                    dig = 0;
-                }
-                printf("%c", (dig <= 0) ? '.' : ((dig < 10) ? '0' + dig : 'A' + dig - 10)); // 有効桁を特殊な手法で表示
-                if (i % 100 == 99) {
-                    printf("\n");
-                }
+            if (map) {                                                                       /* 有効桁数マップの作成 */
+                dig = (long)(-log(p) / log(10.0) + 0.5);                                     // 少数以下の桁数を計算
+                if (dig > 35) dig = 35;                                                      // 35よりも大きければ35に縮める
+                if (dig < 0) dig = 0;                                                        // 0未満なら0に収める
+                printf("%c", (dig <= 0) ? '.' : ((dig < 10) ? '0' + dig : 'A' + dig - 10));  // 有効桁を特殊な手法で表示
+                if (i % 100 == 99) printf("\n");
             }
-            vecX[i] = vecT[i]; // 次の解を暫定会に代入
+            vecX[i] = vecT[i];  // 次の解を暫定会に代入
         }
         if (map) {
             printf("\n");
